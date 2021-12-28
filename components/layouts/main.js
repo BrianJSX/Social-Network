c1654809fa3ebba9c7d4618ce4521bc06653ce0d
Ho Minh <Minhho.technology@gmail.com>
2021-12-28 16:41:36 +0700

    finish loadmore post

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -8,14 +8,19 @@ export default function MainLayout({ children }) {
       <Head>
         <title>Hutech Online</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://unpkg.com/nprogress@0.2.0/nprogress.js"></script>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/nprogress@0.2.0/nprogress.css"
+        />
       </Head>
 
       {/* Header */}
       <Header></Header>
-
-      {/* Children component */}
-      {children}
-      
+      <div id="parent">
+        {/* Children component */}
+        {children}
+      </div>
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,35 @@ import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import EmptyLayout from "../components/layouts/empty";
 import "emoji-mart/css/emoji-mart.css";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import { store } from "../app/store";
 import { Provider } from "react-redux";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
+import React, { useState } from "react";
+import { useRouter } from "next/dist/client/router";
+import NProgress from "nprogress";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const Layout = Component.Layout ?? EmptyLayout;
-  
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  NProgress.configure({ parent: '#parent', showSpinner: false });
+
+  router.events?.on("routeChangeStart", () => {
+    NProgress.start();
+    setLoading(true);
+  });
+
+  router.events?.on("routeChangeComplete", () => {
+    NProgress.done();
+    setLoading(false);
+  });
+
   return (
     <Provider store={store}>
       <SessionProvider session={session}>
         <Layout>
-          <Component {...pageProps} />
+          {!loading && <Component {...pageProps} />}
           <ToastContainer />
         </Layout>
       </SessionProvider>
